ccae51449aaab2801a97586da152c7efdab5c385
CEO_koye <chineduokoye2@gmail.com>
2023-03-19 23:42:52 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * main -: ain function
+ * main - Entry point
  *
- * Return - always 0
- **/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
