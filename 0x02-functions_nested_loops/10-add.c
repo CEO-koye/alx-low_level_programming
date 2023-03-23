96a737f6d4dd3ca28c3361e1beb72a0dfd2f7183
CEO_koye <chineduokoye2@gmail.com>
2023-03-23 02:04:52 +0000

    natural

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @n1: first
+ * @n2: second
+ * Return: integer
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - sum 3 5 1024 multiples
+ * Return: Always (success)
+ */
+int main(void)
+
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+		z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
