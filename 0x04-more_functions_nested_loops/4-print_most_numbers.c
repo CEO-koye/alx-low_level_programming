19b2214010cb903f619e12f481b7bc9a0548800b
CEO_koye <chineduokoye2@gmail.com>
2023-03-24 13:48:24 +0000

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - to print numbers 0 through 9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i != 50 && != 52; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
