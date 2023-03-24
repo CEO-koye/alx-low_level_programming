239f87e634d9f28cf8d230fb209789f266cf900b
CEO_koye <chineduokoye2@gmail.com>
2023-03-24 13:30:41 +0000

    0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - to print numbers 0 through 9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
