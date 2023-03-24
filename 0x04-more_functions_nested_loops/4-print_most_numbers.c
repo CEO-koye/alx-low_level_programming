4dd00d9ab1909386255f199d206fed08711e3d7f
CEO_koye <chineduokoye2@gmail.com>
2023-03-24 14:01:01 +0000

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)i != 50 && != 52)
+	for (i = 48; i <= 57; i++)
 	{
-		(i !(== 50 || == 52))
+		if (!(c == 50 || c == 52))
 			_putchar(i);
 	}
 	_putchar('\n');
