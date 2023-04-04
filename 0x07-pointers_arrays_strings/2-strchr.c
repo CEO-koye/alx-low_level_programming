fc9b6b61cfbbf14cfdaf911572070f94aba5d042
CEO_koye <chineduokoye2@gmail.com>
2023-04-04 22:36:17 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - 
+ * @s: char input
+ * @c: char input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= ''; i++)
+	{
+	if (s[i] == c)
+	return (s+i);
+	}
+	return (0);
+}
+
